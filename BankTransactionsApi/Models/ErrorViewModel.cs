4776a622c938948fb617bc51aa7428b0dc124974
Jasur <Jasur@DESKTOP-AABT8NI>
2024-01-31 21:08:22 -0500

    Add project files.

---
+++ BankTransactionsApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BankTransactionsApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
